9c88f02adf922d86f50a3ea950b3f9adf00d4c7a
Laurent Fallet <laurent.fallet@gmail.com>
2017-09-17 17:54:08 +0200

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'speedtest',
+      script    : 'bin/speed.js',
+      /*env: {
+        COMMON_VARIABLE: 'true'
+      },*/
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'chabe',
+      host : 'eq-cha-node',
+      ref  : 'origin/master',
+      repo : 'git@github.com:ChabeLimousines/speedtest.git',
+      path : '/home/chabe/applications/speedtest',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
+
