68a9ca20de5551766386e23e77ac481ca6c80a56
Laurent Fallet <laurent.fallet@gmail.com>
2017-09-17 17:58:32 +0200

    adding nvm declaration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:ChabeLimousines/speedtest.git',
       path : '/home/chabe/applications/speedtest',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : '/etc/profile.d/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
